e72adc80e78b06314db104fd6fd6d992ac8e433d
Christopher Burrows <s5085838@g23p220m061997.local>
2019-04-09 13:18:45 +1000

    Initial Commit

---
+++ Assingment2_1Tests/Assingment2_1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Assingment2_1Tests.swift
+//  Assingment2_1Tests
+//
+//  Created by Christopher Burrows on 9/4/19.
+//  Copyright © 2019 Christopher Burrows. All rights reserved.
+//
+
+import XCTest
+@testable import Assingment2_1
+
+class Assingment2_1Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
